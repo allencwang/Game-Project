5ecc681b202f055f3ec722d8c7bd14cceb2d889b
Allen Wang <acwang2@uci.edu>
2022-04-09 14:55:31 -0700

    Added dataabase connectivity

--- app.js
+++ app.js
@@ -1,13 +1,31 @@
 // server/index.js
 
+const { ObjectId } = require("bson");
+const { query } = require("express");
 const express = require("express");
+const mongoose = require("mongoose");
 
 const PORT = process.env.PORT || 3001;
 
 const app = express();
 
+mongoose.connect("mongodb+srv://acwang2:5bAKb10OwMLGCO5I@cluster0.9qevw.mongodb.net/sample_mflix?retryWrites=true&w=majority");
+const db = mongoose.connection;
+db.on("error", console.error.bind(console, "connection error: "));
+db.once("open", function () {
+    var Comments = mongoose.model("Comments", new mongoose.Schema({name: String, email: String, movieId: ObjectId, text: String, }));
+    var commentsQuery = Comments.findOne({name: "Mercedes Tyler"});
+    commentsQuery.select('name email');
+    commentsQuery.exec(function(err, result){
+        app.get("/databaseTest", (req, res) => {
+            res.json({ message: "Database connect successfully. Here is Mercedes Tyler's email " + result.email });
+          });
+    })
+});
+
+
 app.get("/api", (req, res) => {
-    res.json({ message: "Hello from server!" });
+    res.json({ message: "Hello World!" });
   });
   
   app.listen(PORT, () => {
