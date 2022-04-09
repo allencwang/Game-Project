a717a2e0a61dcfb5fe70e276238d980fbcbeb94c
Paul Lee <Paul.Lee@teknowlogi.com>
2022-04-09 18:43:32 -0500

    Connection string change

--- app.js
+++ app.js
@@ -9,25 +9,38 @@ const PORT = process.env.PORT || 3001;
 
 const app = express();
 
-mongoose.connect("mongodb+srv://acwang2:5bAKb10OwMLGCO5I@cluster0.9qevw.mongodb.net/sample_mflix?retryWrites=true&w=majority");
+mongoose.connect(
+  "mongodb+srv://paullee:L15OvEuNJMOwGBE7@gameproject.jjlux.mongodb.net/sample_mflix?retryWrites=true&w=majority"
+);
 const db = mongoose.connection;
 db.on("error", console.error.bind(console, "connection error: "));
 db.once("open", function () {
-    var Comments = mongoose.model("Comments", new mongoose.Schema({name: String, email: String, movieId: ObjectId, text: String, }));
-    var commentsQuery = Comments.findOne({name: "Mercedes Tyler"});
-    commentsQuery.select('name email');
-    commentsQuery.exec(function(err, result){
-        app.get("/databaseTest", (req, res) => {
-            res.json({ message: "Database connect successfully. Here is Mercedes Tyler's email " + result.email });
-          });
+  var Comments = mongoose.model(
+    "Comments",
+    new mongoose.Schema({
+      name: String,
+      email: String,
+      movieId: ObjectId,
+      text: String,
     })
+  );
+  var commentsQuery = Comments.findOne({ name: "Mercedes Tyler" });
+  commentsQuery.select("name email");
+  commentsQuery.exec(function (err, result) {
+    app.get("/databaseTest", (req, res) => {
+      res.json({
+        message:
+          "Database connect successfully. Here is Mercedes Tyler's email " +
+          result.email,
+      });
+    });
+  });
 });
 
-
 app.get("/api", (req, res) => {
-    res.json({ message: "Hello World!" });
-  });
-  
-  app.listen(PORT, () => {
-    console.log(`Server listening on ${PORT}`);
-  });
+  res.json({ message: "Hello World!" });
+});
+
+app.listen(PORT, () => {
+  console.log(`Server listening on ${PORT}`);
+});
